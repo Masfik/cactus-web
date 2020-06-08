ca5e85775667b7db81e3ead3dde972b7b6956db6
Masfik <contact@masfik.net>
2020-06-08 22:45:45 +0100

    Added Firebase dependency and FontAwesome brand icons

--- src/plugins/fontawesome.js
+++ src/plugins/fontawesome.js
@@ -3,7 +3,9 @@ import Vue from "vue";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faFontAwesome as faBrands } from "@fortawesome/free-brands-svg-icons";
 
 library.add(fas);
+library.add(faBrands);
 
 Vue.component("font-awesome-icon", FontAwesomeIcon);
