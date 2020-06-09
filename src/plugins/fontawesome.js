724b1d4ce4fd60bdf5004d461cbfa3c9ad5b2879
Masfik <contact@masfik.net>
2020-06-09 07:31:29 +0100

    Added social icons (for sign ups) * Fixed font-awesome brand icons * Refactored code

--- src/plugins/fontawesome.js
+++ src/plugins/fontawesome.js
@@ -3,9 +3,9 @@ import Vue from "vue";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { faFontAwesome as faBrands } from "@fortawesome/free-brands-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
 
 library.add(fas);
-library.add(faBrands);
+library.add(fab);
 
 Vue.component("font-awesome-icon", FontAwesomeIcon);
--- src/views/Authentication.vue
+++ src/views/Authentication.vue
@@ -2,9 +2,22 @@
   <div id="login">
     <div id="login-box">
       <Logo color="var(--c-text-on-bg)" class="logo" />
-
       <h2>Sign Up</h2>
-      <form class="styled-form">
+      <div class="social-icons col">
+        <font-awesome-icon
+          :icon="['fab', 'twitter-square']"
+          style="color: #1B9CEA"
+        />
+        <font-awesome-icon
+          :icon="['fab', 'facebook-square']"
+          style="color: #4466AE"
+        />
+        <font-awesome-icon
+          :icon="['fab', 'github-square']"
+          style="color: #24292e"
+        />
+      </div>
+      <form class="default-style-form">
         <input type="text" placeholder="First Name" maxlength="32" required />
         <input type="text" placeholder="Last Name" maxlength="32" required />
         <input type="text" placeholder="Username" maxlength="32" required />
@@ -66,6 +79,19 @@ export default {
       display: block;
       width: 65%;
       margin: 0 auto 50px auto;
+    }
+
+    .social-icons {
+      margin-bottom: 10px; /* TODO: Use a less random value */
+      font-size: 42px;
+
+      > .svg-inline--fa {
+        vertical-align: initial;
+
+        &:not(:last-of-type) {
+          margin-right: 10px;
+        }
+      }
     }
   }
 }
