38c2452a26eb3ae2bf6fd88a054c7662d57bae95
Masfik <contact@masfik.net>
2020-07-04 04:37:28 +0100

    Using MessageBubbleBlock component in Chat.vue - Removed block-related code from Chat.vue

--- src/components/room/chat/Chat.vue
+++ src/components/room/chat/Chat.vue
@@ -3,47 +3,11 @@
     <room-name-header />
     <div id="message-list-container">
       <div id="msg-list-scrollable">
-        <div class="recipient">
-          <img src="@/assets/user.png" alt="User avatar" />
-          <div class="message-container">
-            <div class="chat-who">MimmiMH</div>
-            <div class="messages">
-              <message-bubble :message="{ data: 'Hello!' }" />
-            </div>
-          </div>
-        </div>
-        <div class="sender">
-          <div class="message-container">
-            <div class="messages">
-              <message-bubble :message="{ data: 'Aaaaa' }" />
-              <message-bubble :message="{ data: 'Two messages' }" />
-            </div>
-          </div>
-        </div>
-        <div class="recipient">
-          <img src="@/assets/user.png" alt="User avatar" />
-          <div class="message-container">
-            <div class="chat-who">Giuseppe</div>
-            <div class="messages">
-              <message-bubble :message="{ data: 'Hello Spongebob' }" />
-              <message-bubble :message="{ data: 'Testing' }" />
-              <message-bubble :message="{ data: 'How are you?' }" />
-              <message-bubble :message="{ data: 'How was work?' }" />
-              <message-bubble
-                :message="{ data: 'I hope everything is going well for ya.' }"
-              />
-            </div>
-          </div>
-        </div>
-        <div class="recipient">
-          <img src="@/assets/user.png" alt="User avatar" />
-          <div class="message-container">
-            <div class="chat-who">Tommaso</div>
-            <div class="messages">
-              <message-bubble :message="{ data: lorem }" who="recipient" />
-            </div>
-          </div>
-        </div>
+        <message-bubbles-block
+          v-for="block of messageBlocks"
+          :key="block.id"
+          :block="block"
+        />
       </div>
     </div>
     <send-message-bar />
@@ -51,19 +15,45 @@
 </template>
 
 <script lang="ts">
-import MessageBubble from "@/components/room/chat/MessageBubble.vue";
 import SendMessageBar from "@/components/room/chat/SendMessageBar.vue";
 import RoomNameHeader from "@/components/room/chat/RoomNameHeader.vue";
+import MessageBubblesBlock from "@/components/room/chat/MessageBubblesBlock.vue";
+import { computed, SetupContext } from "@vue/composition-api";
+import { Message } from "@/models/message";
+import { User } from "@/models/user";
+import { Room } from "@/models/room";
+import { UserStatus } from "@/models/user-status";
 
 export default {
   name: "Chat",
-  components: { RoomNameHeader, SendMessageBar, MessageBubble },
-  data() {
+  components: { MessageBubblesBlock, RoomNameHeader, SendMessageBar },
+  setup(_: any, ctx: SetupContext) {
+    const { $store } = ctx.root;
+
+    const messageBlocks = computed(
+      () => ($store.state.roomStore.currentRoom as Room).messageBlocks
+    );
+
+    const mimmi: User = {
+      friends: [],
+      id: "45",
+      name: "Mimmi",
+      surname: " 🧼",
+      rooms: [],
+      status: UserStatus.ONLINE,
+      username: "MimmiMH"
+    };
+
+    setInterval(() => {
+      $store.dispatch("roomStore/addMessage", {
+        id: Math.random().toString(),
+        from: mimmi,
+        data: `Some reeeeally random message. ${Math.random().toString()}`
+      } as Message);
+    }, 5000);
+
     return {
-      lorem:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus atque deleniti dolores excepturi, " +
-        "ipsa modi natus neque officia quaerat, quidem, sed velit vitae voluptate? Dicta doloremque excepturi " +
-        "minus numquam voluptas!"
+      messageBlocks
     };
   }
 };
@@ -88,50 +78,6 @@ export default {
     flex-grow: 1;
     display: flex;
     flex-flow: column;
-  }
-
-  .recipient,
-  .sender {
-    &:first-child {
-      margin-top: auto;
-    }
-  }
-
-  .recipient {
-    display: flex;
-    margin-top: $s-message-block-spacing;
-
-    img {
-      @include circular-avatar(36px);
-      align-self: flex-end;
-      margin-right: 6px;
-    }
-
-    .chat-who {
-      @extend %text-ellipsis;
-      max-width: 150px;
-      margin-left: 6px;
-      font-size: smaller;
-      font-weight: bold;
-    }
-
-    > div.message-container > div.messages > *.message-bubble {
-      @include bubble-borders(recipient);
-      background: var(--c-bubble-recipient);
-      color: var(--c-text-on-primary);
-    }
-  }
-
-  .sender {
-    margin-top: $s-message-block-spacing;
-
-    > div.message-container > div.messages > *.message-bubble {
-      @include bubble-borders(sender);
-      margin-left: auto;
-      margin-right: 0;
-      background: var(--c-bubble-sender);
-      color: var(--c-text-on-primary);
-    }
   }
 }
 </style>
