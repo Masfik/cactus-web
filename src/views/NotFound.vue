b328b15a5e08f44c5e61bd238799b0b168e56690
samtolomeo <Masfik12>
2020-07-15 03:44:43 +0200

    Made 404 page look cool

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,5 +1,15 @@
 <template>
-  <h1>404 - Not Found</h1>
+  <div class="not-found-container">
+    <div>
+      <img src="/assets/img/undraw_Taken_if77.svg" />
+      <br>
+      <h1>Whoopsie!</h1>
+      <small>
+        Looks like the page you're looking for<br>doesn't exist.<br><br>
+        Or maybe it has just been stolen<br>by some alien cactus 🌵
+      </small>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,11 +18,36 @@ export default {
 };
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 h1 {
   margin: 0;
-  line-height: 100vh;
-  font-size: 10vw;
+  font-size: 3vw;
   text-align: center;
+}
+
+img {
+  width: 260px;
+  user-select: none;
+  pointer-events: none;
+  -webkit-user-drag: none;
+  margin-bottom: 30px;
+  display: inline-block;
+}
+
+
+.not-found-container {
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  text-align: center;
+
+  div {
+    small {
+      margin-top: 10px;
+      display: inline-block;
+    }
+  }
 }
 </style>
