7e54d92e90e765400a49d1e41de78455ea550dc3
Masfik <contact@masfik.net>
2020-08-01 21:46:47 +0100

    Created simple EventHandler class

---
+++ src/utils/event-handler.ts
@@ -0,0 +1,34 @@
+/**
+ * A simple event handler class to listen and emit events.
+ * Note that an event can only be listened once. If an event is listened
+ * multiple times, only the latest registered callback will be executed.
+ *
+ * Type T refers to the list of events accepted by the handler.
+ * Type U refers to the callback.
+ */
+export class EventHandler<T extends string, U extends Function> {
+  private events: {
+    [event: string]: U;
+  } = {};
+
+  /**
+   * Listen to the specified event and execute the given callback whenever
+   * the event is triggered.
+   *
+   * @param event
+   * @param callback
+   */
+  on(event: T, callback: U): void {
+    this.events[event] = callback;
+  }
+
+  /**
+   * Emit an event with the given data.
+   *
+   * @param event
+   * @param data
+   */
+  emit(event: T, ...data: any): void {
+    if (this.events[event]) this.events[event](...data);
+  }
+}
